22f814ddcedf498bedb6d9cd9edf4364fb45c522
Stas S <theavalanche@yandex.ru>
2026-09-18 22:47:11 +0300

    2026 09 17 add audio_service to startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:audio_flow/hive/hive_registrar.g.dart' show HiveRegistrar;
 import 'package:audio_flow/src/bloc/audio_player_bloc.dart';
+import 'package:audio_flow/src/configuration/audio_service.dart' show initAudioFlowAudioService;
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 
@@ -15,10 +16,12 @@ void main() async {
   await settings.initLazyBox();
   // settings.clearAllSettings();
   await initLogger();
-  final audioPlayerBloc = AudioPlayerBloc();
   settings.audioSession = await settings.initAudioSession();
   settings.setPlayerStatus(AudioStatus.initial);
   await settings.initSoloud();
+  logger.log.d('Soloud state is : ${settings.soloud.isInitialized}');
+  final audioPlayerBloc = AudioPlayerBloc();
+  await initAudioFlowAudioService(audioPlayerBloc: audioPlayerBloc);
   logger.log.d('Application started');
   logger.logNS.d('Let\'s go!!!');
   runApp(AudioFlowApp(audioPlayerBloc: audioPlayerBloc));
--- lib/src/configuration/audio_service.dart
+++ lib/src/configuration/audio_service.dart
@@ -1,19 +1,60 @@
+import 'package:audio_flow/src/bloc/audio_player_bloc.dart';
 import 'package:audio_service/audio_service.dart';
 
-class AudioFlowService {
-  late AudioHandler audioHandler; // singleton.
-  
-  void initAudioService() async {
-    audioHandler = await AudioService.init(
-      builder: () => BaseAudioHandler(),
-      config: const AudioServiceConfig(
-        androidNotificationChannelId: 'com.example.audio_flow.audio',
-        androidNotificationChannelName: 'Audio Service Demo',
-        androidNotificationOngoing: true,
-        androidStopForegroundOnPause: true,
-      ),
-    );
-  }
+Future<AudioHandler> initAudioFlowAudioService({required AudioPlayerBloc audioPlayerBloc}) async {
+  return await AudioService.init(
+    builder: () => AudioFlowAudioHandler(audioPlayerBloc: audioPlayerBloc),
+    config: const AudioServiceConfig(
+      // androidNotificationChannelId: 'com.example.audio_flow.channel.audio',
+      androidNotificationChannelName: 'AudioFlow Playback',
+      androidNotificationOngoing: true,
+      androidShowNotificationBadge: true,
+    ),
+  );
 }
 
-final audioFlowService = AudioFlowService();
+class AudioFlowAudioHandler extends BaseAudioHandler with SeekHandler {
+  final AudioPlayerBloc audioPlayerBloc;
+  AudioFlowAudioHandler({required this.audioPlayerBloc});
+
+
+  @override
+  Future<void> play() async => audioPlayerBloc.add(AudioPlayerStartPlayEvent());
+
+  @override
+  Future<void> pause() async => audioPlayerBloc.add(AudioPlayerPauseEvent());
+
+  @override
+  Future<void> stop() async => audioPlayerBloc.add(AudioPlayerStopEvent());
+
+
+  /// Transforms just_audio events into audio_service PlaybackStates
+  // PlaybackState transformEvent(AudioPlayerState state) {
+  //   return PlaybackState(
+  //     controls: [
+  //       MediaControl.rewind,
+  //       if (_player.playing) MediaControl.pause else MediaControl.play,
+  //       MediaControl.stop,
+  //       MediaControl.fastForward,
+  //     ],
+  //     systemActions: const {
+  //       MediaAction.seek,
+  //       MediaAction.seekForward,
+  //       MediaAction.seekBackward,
+  //     },
+  //     androidCompactActionIndices: const, // Show play/pause in compact notification view
+  //     processingState: const {
+  //       ProcessingState.idle: AudioProcessingState.idle,
+  //       ProcessingState.loading: AudioProcessingState.loading,
+  //       ProcessingState.buffering: AudioProcessingState.buffering,
+  //       ProcessingState.ready: AudioProcessingState.ready,
+  //       ProcessingState.completed: AudioProcessingState.completed,
+  //     }[_player.processingState]!,
+  //     playing: _player.playing,
+  //     updatePosition: _player.position,
+  //     bufferedPosition: _player.bufferedPosition,
+  //     speed: _player.speed,
+  //     queueIndex: event.currentIndex,
+  //   );
+  // }
+}
